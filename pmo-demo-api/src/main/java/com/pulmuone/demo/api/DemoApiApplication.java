fc57271cea9c29e5272237e4a2dee3df50efa559
kjlee <kjlee@kjleeui-MacBookPro.local>
2019-06-21 14:07:21 +0900

    s3 upload

--- pmo-demo-api/src/main/java/com/pulmuone/demo/api/DemoApiApplication.java
+++ pmo-demo-api/src/main/java/com/pulmuone/demo/api/DemoApiApplication.java
@@ -1,23 +1,18 @@
 package com.pulmuone.demo.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = { "com.pulmuone.demo" })
+@PropertySource(value = "classpath:aws.properties")
 public class DemoApiApplication extends SpringBootServletInitializer {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:aws.properties";
-
     public static void main(String[] args) {
-
-        new SpringApplicationBuilder(DemoApiApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(DemoApiApplication.class, args);
     }
 
 
